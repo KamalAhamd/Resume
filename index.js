dca9a5929e00c146ed5f09fa32e2327ccd0c6184
KamalAhamd <103666396+KamalAhamd@users.noreply.github.com>
2022-06-21 11:10:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const rightnavi = document.querySelector("#rightNavi");
-
+const ul = document.querySelector("ul");
 function showMenu() {
   rightnavi.style.right = "0";
+  rightnavi.style.display = "block";
 }
 
 function hideMenu() {
   rightnavi.style.right = "-300px";
+  rightnavi.style.display = "none";
 }
